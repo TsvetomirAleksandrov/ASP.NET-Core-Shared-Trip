a940d87a5cdc8017867cdeef2caf1c5712da8e23
alexandrovbg <cecko.aleksandrov@gmail.com>
2021-01-29 19:12:55 +0200

    DbSets added

--- Apps/SharedTrip/Data/ApplicationDbContext.cs
+++ Apps/SharedTrip/Data/ApplicationDbContext.cs
@@ -13,6 +13,12 @@
         {
         }
 
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Trip> Trips { get; set; }
+
+        public DbSet<UserTrip> UserTrips { get; set; }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             if (!optionsBuilder.IsConfigured)
