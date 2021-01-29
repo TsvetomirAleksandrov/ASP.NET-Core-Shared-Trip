47b398eb7453b2ec985516b20ea3febaf5c6ea16
alexandrovbg <cecko.aleksandrov@gmail.com>
2021-01-29 21:56:44 +0200

    update

---
+++ Apps/SharedTrip/Services/TripsService.cs
@@ -0,0 +1,33 @@
+﻿using SharedTrip.Data;
+using SharedTrip.ViewModels.Trips;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharedTrip.Services
+{
+    public class TripsService : ITripsService
+    {
+        private readonly ApplicationDbContext db;
+
+        public TripsService(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
+
+        public void Create(AddTripInputModel trip)
+        {
+            var dbTrip = new Trip
+            {
+                DepartureTime = trip.DepartureTime,
+                Description = trip.Description,
+                EndPoint = trip.EndPoint,
+                ImagePath = trip.ImagePath,
+                Seats = (byte)trip.Seats,
+                StartingPoint = trip.StartPoint,
+            };
+            this.db.Trips.Add(dbTrip);
+            this.db.SaveChanges();
+        }
+    }
+}
